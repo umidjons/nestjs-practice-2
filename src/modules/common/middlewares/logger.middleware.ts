db566fe2ebc54d2fed9fc3144403b203082a88ce
Umid Almatov <almatov.us@gmail.com>
2017-10-14 15:21:41 +0500

    Asynchronous middleware example

--- src/modules/common/middlewares/logger.middleware.ts
+++ src/modules/common/middlewares/logger.middleware.ts
@@ -2,9 +2,21 @@ import { Middleware, NestMiddleware, ExpressMiddleware } from '@nestjs/common';
 
 @Middleware()
 export class LoggerMiddleware implements NestMiddleware {
-  resolve(name: string): ExpressMiddleware {
-    return (req, res, next) => {
+  test(): Promise<string> {
+    return Promise.resolve('Test string from async function');
+  }
+
+  async resolve(name: string): Promise<ExpressMiddleware> {
+    // make async operation
+    let message = await this.test();
+    console.log('Async operation in initialization stage:', message);
+
+    return async(req, res, next) => {
       console.log(`[${name}] Request...`);
+
+      let message = await this.test();
+      console.log('Async operation inside a middleware:', message);
+
       next();
     };
   }
