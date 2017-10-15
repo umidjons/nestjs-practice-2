574aab340f6b207418ab919d2909e97c6d2f9a4a
Umid Almatov <almatov.us@gmail.com>
2017-10-15 17:14:03 +0500

    e2e test example.

---
+++ src/e2e/cats.e2e-spec.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { CatsModule } from '../modules/cats/cats.module';
+import { CatsService } from '../modules/cats/cats.service';
+
+describe('Cats', ()=>{
+  const server = express();
+  server.use(bodyParser.json());
+
+  const catsService = {
+    findAll(){ return ['test']; }
+  };
+
+  beforeAll(async()=>{
+    const module = await Test.createTestingModule({
+      modules: [CatsModule]
+    })
+    .overrideComponent(CatsService).useValue(catsService)
+    .compile();
+
+    const app = module.createNestApplication(server);
+    await app.init();
+  });
+
+  it('GET /cats', ()=>{
+    return request(server)
+      .get('/cats?uid=0')
+      .expect(200)
+      .expect({
+        data: catsService.findAll()
+      });
+  });
+});
